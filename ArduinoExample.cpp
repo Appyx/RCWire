8948583d16bb294a768f5d2728697f00461aaf4a
Appyx <robert.gstoettner@icloud.com>
2016-08-24 01:08:28 +0200

    blank readme

--- ArduinoExample.cpp
+++ ArduinoExample.cpp
@@ -1,3 +1,31 @@
+/**
+ * RCWire - Arduino and RaspberryPi library for sending messages with 315/433MHz AM transmitter.
+ *
+ * Copyright (c) 2011 Gstöttner Robert.  All right reserved.
+ *
+ * Project home: https://github.com/Appyx/RCWire
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * This library is built on top of RCSwitch (https://github.com/sui77/rc-switch).
+ * The modifications to the original library are marked as comments in the source.
+ *
+ */
+
 #include "Arduino.h"
 #include "WirelessWire.h"
 
